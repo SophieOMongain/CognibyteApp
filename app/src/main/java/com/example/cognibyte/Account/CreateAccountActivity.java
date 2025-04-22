96c6d04d620170b2ec16e1fd2540479364b5969b
SophieOMongain <146173098+SophieOMongain@users.noreply.github.com>
2025-04-22 21:11:03 +0100

    test

--- app/src/main/java/com/example/cognibyte/Account/CreateAccountActivity.java
+++ app/src/main/java/com/example/cognibyte/Account/CreateAccountActivity.java
@@ -18,7 +18,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
-//this is a test to check if github is working
+
 public class CreateAccountActivity extends AppCompatActivity {
 
     private EditText etName, etEmail, etUsername, etPassword, etDateOfBirth;
